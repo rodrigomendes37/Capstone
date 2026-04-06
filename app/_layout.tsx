6bfded895e282a9cc59b016de9f3c4ab3eedd122
rodrigomendes37 <rmaiamen@unca.edu>
2026-04-06 14:19:18 -0400

    Disable runtime notification calls for crash isolation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,14 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { useEffect } from "react";
-import { scheduleDailyCheckInReminder } from "../lib/services/notifications";
 
 export default function RootLayout() {
-
-  useEffect(() => {
-    scheduleDailyCheckInReminder().catch((err) => {
-      console.log("Daily reminder scheduling error:", err);
-    });
-  }, []);
 
   return (
     <SafeAreaProvider>
       <Stack screenOptions={{ headerShown: false }}>
-        {/* Login screen */}
+
         <Stack.Screen name="login" />
 
-        {/* Tabs group */}
         <Stack.Screen name="(tabs)" />
       </Stack>
     </SafeAreaProvider>
